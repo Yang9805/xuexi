becbe3c2e3e1162fac124f366733c405e5f395a7
liuyang <liuyang@itcast.cn>
2025-08-08 14:47:07 +0800

    新概念英语1-2,3-4的完善

--- EnglishStudy/src/新概念英语1/lseon1to2.java
+++ EnglishStudy/src/新概念英语1/lseon1to2.java
@@ -5,12 +5,69 @@ public class lseon1to2 {
    上一次复习时间：2025年8月8日10:19:22
    1.听次数:
    2.说次数：
-   3.读次数：
+   3.读次数：lseon1:118;
+           lseon2:26;
    4.写次数：
-   5.文章主要语法学习次数：
-   6.复习次数
+   5.文章主要内容学习次数：
 
    二，主要内容：
+        1.单词讲解
+leson1：
+            01. Excuse    原谅                 单词的字母c被浊化发出”咯“g的音
+
+            02. me        我        宾格
+                eg：     主格：I,宾格：me
+                        主格：you,宾格：you
+                        主格：she,宾格：her
+                        主格：he,宾格：him
+                        主格：it,宾格：it
+
+            03. yes     是的
+
+            04. is      be动词现在时，第三人称单数（is，am，are）
+
+            05. this    这，这个
+
+            06. your    你的
+
+            07. handbag 手提包 hand 手 bag 包
+
+            08. pardon 原谅，再说一遍
+
+            09. it    它，可以替代物体，时间，天气
+
+            10. thank you 谢谢你
+
+            11.very musch  非常地
+leson2：
+            01. pen :钢笔
+            02. pencil ：铅笔    felt pen水彩笔
+            03. book ：书      a good book   ，a new book ，book1
+            04. watch 手表     a new watch   ，Swatch瑞士手表品牌
+            05. coat 外套
+            06. dress 连衣裙
+	        07. skirt 短裙
+	        08. shirt 衬衫     a white shirt
+	        09. car 小车   bus公共汽车 ，coach长途汽车
+	        10. house 大房子   公寓楼：flat（英）apartment（美）
+
+
+
+
+
+
+
+        2.语法讲解
+            01.一般疑问句
+                功能：询问
+                构成：be动词至于句前（am，is，are）
+              eg：
+                I am great。变成一般疑问句：Are you great？
+                You are my friend。变成一般疑问句：Are you my friend？
+                She is pretty。变成一般疑问句： Is she pretty?
+
+            02. Is this your......？    这是你的吗？
+
 
 
  */
