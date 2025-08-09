fedc286ce6c58f60c37a656163eaf41a518c053b
liuyang <liuyang@itcast.cn>
2025-08-09 11:21:26 +0800

    新概念英语leson9-10的学习笔记

--- EnglishStudy/src/新概念英语1/lseon9to10.java
+++ EnglishStudy/src/新概念英语1/lseon9to10.java
@@ -2,65 +2,104 @@ package 新概念英语1;
 
 public class lseon9to10 {
     /*课程基础
-   上一次复习时间：
+   上一次复习时间：2025年8月9日11:18:02
    1.听次数:
    2.说次数：
    3.读次数：
    4.写次数：
-   5.文章主要内容学习次数：
+   5.文章主要内容学习次数：1
    6.不足：
    7.修正：
 
    二，主要内容：
 leson9
     1.单词讲解
-      01.
-      02.
-      03.
-      04.
-      05.
-      06.
-      07.
-      08.
-      09.
-      10.
-      11.
-      12.
-      13.
-      14.
-      15.
-      16.
-      17.
-      18.
-      19.
-      20.
+      01. hello      你好
+            hey
+            hi
+
+      02. how       怎样，如何
+            How are you？你怎么样
+
+      03. today  今天
+            tonight 今晚
+
+      04. well 身体好
+
+      05.fine  状况良好
+
+      06.thanks  感谢
+            thanks a lot 多谢
+            thank you very much
+
+      07.good bye 再见
+
+      08.see 看见
+            see you 回见
+
 leson10
-      01.
-      02.
-      03.
-      04.
-      05.
-      06.
-      07.
-      08.
-      09.
-      10.
-      11.
-      12.
-      13.
-      14.
-      15.
-      16.
-      17.
-      18.
-      19.
-      20.
+      01.  fat 胖的
+            a little heavy 有点重
+
+      02.  woman 女人
+            lady 女士
+
+      03.   thin 瘦的
+            slim 苗条的
+            You are so slim
+
+      04.   tall 高的（人，树，建筑物）
+
+      05.   dirty 脏的
+      06.   clean 干净的
+            Nice and clean 干干净净
+
+      07.   hot 热的
+            It is hot today 今天天气好热
+            The girl is very hot 那个女孩性感
+            ....is hot now...  现在很火
+
+      08.   cold 冷漠的，冷的
+            It is very cold today   今天天气很冷
+            I have a cloud welcome。我收到了冷遇
+
+      09.   old 老的，旧的
+            It is an old story 这个是一个古老的故事
+            That is my old book 那是我的旧书
+
+      10.   young  年轻的
+            He is a young man。 他是一个年轻人
+
+      11.   busy  忙碌的
+            It is a busy today。真是忙碌的一天
+            He is a busy 他特别的忙
+
+      12.   lazy 懒惰的
+
+      13.  look at ...看
+            look at me/her/him..
+            meet you/him/her..
+
+      14.   look at和see的区别
+            look at 是动作，没有看见
+            see    看见，是结果
+            eg：look at  that man，He is very busy。
+                看这个男人，他非常忙碌
+                I see him in the treet。
+                我在街头看见他了
+
+      15. that man和the man 意思完全一样
+               但是the是定冠词后面不能单独用只能接名词
+               that是单独用，后面也可以不接名词
+
     2.语法讲解
-      01.
-      02.
-      03.
-      04.
-      05.
+      01.特殊疑问词 how 怎样，如何
+        How are you？  你怎么样
+        How +to+动词  如何去做某事
+           How to cook rice?如何煮米饭？
+           How to learn English fast?
+           How to fix a bicycle66
+
 
  */
 }
