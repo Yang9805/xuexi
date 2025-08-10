8599169e87bfa4051fdf99be6d5cbad1ac6f0f8a
liuyang <liuyang@itcast.cn>
2025-08-10 09:13:49 +0800

    新概念英语leson13-14的学习笔记

--- EnglishStudy/src/新概念英语1/lseon13to14.java
+++ EnglishStudy/src/新概念英语1/lseon13to14.java
@@ -2,65 +2,80 @@ package 新概念英语1;
 
 public class lseon13to14 {
     /*课程基础
-   上一次复习时间：
+   上一次复习时间：2025年8月10日09:13:36
    1.听次数:
    2.说次数：
    3.读次数：
    4.写次数：
-   5.文章主要内容学习次数：
+   5.文章主要内容学习次数：1
    6.不足：
    7.修正：
 
    二，主要内容：
 leson13
     1.单词讲解
-      01.
-      02.
-      03.
-      04.
-      05.
-      06.
-      07.
-      08.
-      09.
-      10.
-      11.
-      12.
-      13.
-      14.
-      15.
-      16.
-      17.
-      18.
-      19.
-      20.
+      01.color/colour 颜色
+           what color？
+           what color is it？
+           what+名词（color ，make，time，nationality）
+      02.green 绿色
+      03.come 来
+         come here 来这里
+      04.upstairs 楼上
+         come upstairs 上楼来
+         Go downstairs 下楼去
+      05.smart  时髦的，漂亮的
+         beautiful
+      06.hat 帽子
+         cap
+         hood
+      07.same 相同
+         the same color/name/city
+         They have the sane name
+      08.lovely 可爱的，漂亮的
+         beautiful/smart/lovely/nice
+      09.What color is your。。。。？
+      10.Come and see it  过来看看
+         come and meet my mom
+         come and sit down..
+         come and do it
+      11.Here it is =it is here
+         here+主语+be
+         I am here=Here i am
+         He is here =Here he is
+         here we go 咱开始吧
+
 leson14
-      01.
-      02.
-      03.
-      04.
-      05.
-      06.
-      07.
-      08.
-      09.
-      10.
-      11.
-      12.
-      13.
-      14.
-      15.
-      16.
-      17.
-      18.
-      19.
-      20.
+      01.case箱子
+         suitcase 手提箱
+      02.carpet 地毯
+         car汽车+pet宠物=carpet地毯
+      03.dog狗
+         puppy小狗
+         pup
+         a lucky dog 幸运儿
+      04.waht color is your。。。？umbrella/car/shirt/coat/case/carpet/blouse/tie
+
     2.语法讲解
-      01.
-      02.
-      03.
-      04.
-      05.
+      01.特殊疑问词
+          waht，whose，how
+          语序： 特殊疑问句+be+名词 /特殊疑问词+名词+be+...
+          what
+             特殊疑问句+be+名词
+             what is your name?
+             特殊疑问词+名词（make，time，nationality，color）+be+...
+             what color is your car?
+          whose
+             特殊疑问句+be+名词
+             特殊疑问词+名词+be+...
+             whose is your shirt?=whose shirt is this?
+             whose is that phone?=whose phone is that?
+          who
+            特殊疑问句+be+名词
+            How are you?      你好吗
+            How is Sam?       谁是sam
+            How is weather?  天气怎么用
+
 
  */
 }
