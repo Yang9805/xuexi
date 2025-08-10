b12f0d2e05b99130d546a9057352a42dec633718
liuyang <liuyang@itcast.cn>
2025-08-10 12:18:05 +0800

    新概念英语leson15-16和17-18的学习笔记

--- EnglishStudy/src/新概念英语1/lseon15to16.java
+++ EnglishStudy/src/新概念英语1/lseon15to16.java
@@ -2,7 +2,7 @@ package 新概念英语1;
 
 public class lseon15to16 {
     /*课程基础
-   上一次复习时间：
+   上一次复习时间：2025年8月10日11:12:07
    1.听次数:
    2.说次数：
    3.读次数：
@@ -14,53 +14,142 @@ public class lseon15to16 {
    二，主要内容：
 leson15
     1.单词讲解
-      01.
-      02.
-      03.
-      04.
-      05.
-      06.
-      07.
-      08.
-      09.
-      10.
-      11.
-      12.
-      13.
-      14.
-      15.
-      16.
-      17.
-      18.
-      19.
-      20.
+      01.custom 海关
+         customer 顾客
+      02. officer 官员
+          customs office 海关官员
+          CEO首席执行官Chief Executive Office
+
+      03.girl 女孩
+          my girl
+          my boy
+      04.Danish 丹麦人
+         Denmark 丹麦
+      05.friend 朋友
+        boyfriend/girlfriend/a good friend
+        a new friend/ an old friend
+      06.Norwegian 挪威人
+         Norway 挪威
+      07.passprot 护照
+         pass通过 prot港口
+      08.visa 签证
+      09.brown 棕色的
+         brown tea 红茶
+         borwn sugar 红糖
+      10.tourist 旅游者 -ist表示人的后缀
+         -ist  dentis /artist/ scientist
+         -er   teacher/worker/driver
+         -or   doctor/director
+      11.Your passprots please。请出示你的护照
+          n.+please。
+          11.1点餐时 A roast duck，please
+          11.2购物时 This shirt，please
+          11.3检票时 Your ID card.please
+      12. We are Danish 我们是丹麦人
+          12.1   我 I   am   我们 we
+                 你 you are  你们 you     都用are
+                 他 he  is   他们 they
+                 她 she is   他们 they
+      13. Are these your cases?  这些是你们的箱子吗
+          Our cases are brown  我们的箱子是棕色的
+          复数关乎整个句子，而非一个单词~
+          这是你的书
+          This is my book
+          这些是我们的书
+          These are our books。
+          那是她的车
+          That is her car
+          那些是他们的车
+          Those are their cars
+      14.我们的 our
+         他们的 their   后面可以加单数也可以加复数
+         our boos/their cars
+         our mum/school
+         their teacher/house
+
+      15.Here they are 给你
+         They are here 他们在这
+      16.That‘s fine  就这样吧！行啦！
+      17.护照：passport
+         现金：cash
+         机票：return ticket
+         在美国的住址：the address in America
+         what’s your purpose of visit？去干嘛
+         For sighteeing/study去旅游/学习
+
 leson16
-      01.
-      02.
-      03.
-      04.
-      05.
-      06.
-      07.
-      08.
-      09.
-      10.
-      11.
-      12.
-      13.
-      14.
-      15.
-      16.
-      17.
-      18.
-      19.
-      20.
+      01.Russian 俄罗斯人
+         Russia 俄罗斯
+      02.Dutch 荷兰人
+         Holland 荷兰
+         Netherland
+      03.these 这些
+         this 这个 these 这些
+         that 那个 those 那些
+         这是他的手表
+         This is his watch
+         这些是他们的手表
+         These are their watches
+      04.red 红色的
+         red lights
+      05.grey 灰色的
+         gray
+         grey hair 花白的头发
+      06. balck 黑色的
+          balck tea 红茶
+          balck coffee 纯咖啡
+          black man 黑人
+      07.yellow 黄色的
+      08.orange 橘黄色
+
     2.语法讲解
-      01.
-      02.
-      03.
-      04.
-      05.
+      01.名词的单复数
+        1.1 单数：1
+            复数：2或以上
+            a car-two cars
+            a bag-three bags
+            名词n.1
+        1.2单词变成复数的规则
+           1.一般名词+s
+             a girl-girls
+             an officer-officers
+             a coat-coats
+             a boy-boys
+            2.以s，sh，x，ch结尾的+es
+             a bus-buses
+             a box-boxes
+             a brush-brushes
+            3.以f，fe结尾的变 f，fe为ves
+             a wife-wives
+             a shelf-shelves
+             a knife-knives
+             a leaf- leaves
+            4.土豆，西红柿，黑人，英雄+es
+              a potato -potatoes
+              a tomato-tomatoes
+              a Negro- Negroes
+              a hero - heroes
+             以o结尾的其他 +s
+               a zoo -zoos
+               a radio- radios
+               a photo- photos
+           5.辅音字母+y结尾的名词，变y为i+es
+            a baby-babies
+            a lady-ladies
+            a fly-flies
+      02.单数-复数名词在句子中做主语
+         单数+is
+         复数+are
+         The tourists are from Norway
+         The teacaher is American
+         These cars are brown
+         My student are young
+      03.通常以复数出现的词
+         jeans
+         pants
+         shorts
+         glasses
+
 
  */
 }
--- EnglishStudy/src/新概念英语1/lseon17to18.java
+++ EnglishStudy/src/新概念英语1/lseon17to18.java
@@ -14,26 +14,59 @@ public class lseon17to18 {
    二，主要内容：
 leson17
     1.单词讲解
-      01.
-      02.
-      03.
-      04.
-      05.
-      06.
-      07.
-      08.
-      09.
-      10.
-      11.
-      12.
-      13.
-      14.
-      15.
-      16.
-      17.
-      18.
-      19.
-      20.
+      01.employee 员工
+         employ雇佣
+         employer 雇佣者，雇主
+         -er...怎么样的人
+         ee....被怎么的人
+         trainer 培训者
+         trainee 被培训的人
+      02.hard-working 勤奋的
+         构成：adj.+doing
+         good-looking好看的
+         easy-going 好相处的
+      03.sales rep推销员
+         复数 sales reps
+         全称：sales representative 销售员
+      04.man 男人
+         Hi！man！ 哥们最近好啊
+         复数：men
+      05.office 办公室
+         office buildings办公大楼
+         office hours办公时间
+      06.assistant 助手
+         an office assistant办公室助理
+         a shop assistant 商场售货员
+         a classroom assistant 助教
+      07.打招呼
+          hello，hi，hey
+          How are you?
+          How are you doing?
+          How  do you do?非常正式的场合
+      08.come and meet our employees
+      09.waht are their jobs？
+         what is your job？
+         句子的复数形式，名词，助动词，代词都会发生改变
+         This is my name
+         these are their names
+         That is her car
+         Those are their cars
+         He is an assistant
+         They are assistants
+         Who is young girl？
+         Who are young girls？
+         what is your job？
+         what are their jobs？
+         He is a tourist
+         They are tourists
+
+         Who特殊疑问词词，谁
+         特殊疑问词+be+名词
+          那个年轻人是谁
+          who is that young man？
+          那些年轻人是谁
+          who are those young men
+
 leson18
       01.
       02.
@@ -56,11 +89,17 @@ leson18
       19.
       20.
     2.语法讲解
-      01.
-      02.
-      03.
-      04.
-      05.
+      01.名词的单复数
+         1.按规则
+            books，jobs，buses，boxes，brushes，benches
+            wife-wives  leaf-leaves，baby-babies
+         2.不按规则
+            man-men  woman-women
+            foot-feet，tooth-teeth
+            child-children
+            单复同形:sheep.fish
+      02.句子的复数形式
+         单数用is ，复数用are
 
  */
 }
